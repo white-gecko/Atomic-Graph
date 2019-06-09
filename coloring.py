d40c5c2c4894c15f13e74f26c058039d1100bd58
username <sebastianz541@googlemail.com>
2019-06-09 15:47:11 +0200

    completed simple coloring

--- coloring.py
+++ coloring.py
@@ -2,50 +2,56 @@ import rdflib
 
 
 class GraphIsoPartitioner:
-    class ColourHash:
-        def __init__(self):
-            self.hash = {}
-
-    class HashTupel(ColourHash):
-        def hash(colour1, colour2):
+    class HashTupel:
+        def hashT(self, colour1, colour2):
             return colour1 + colour2
 
-    class HashBag(ColourHash):
-        def hash(colour1, colour2):
+    class HashBag:
+        def hashB(self, colour1, colour2):
             array = colour1.split("|") + colour2.split("|")
             array.sort()     # the operation become commutative and associative
             return "|".join(array)
 
     def colour(self, graph):
         colour = {}
-        graph = rdflib.Graph()
         for node in iter(graph.all_nodes()):
             if isinstance(node, rdflib.BNode):
-                colour[node] = ""
+                colour[node] = "[]"
             else:
                 colour[node] = node.n3()
+        # all terms need colour codes
+        for predicate in graph.predicates():
+            colour[predicate] = predicate.n3()
 
         colourPrevious = {}  # init so while condition does not fail
         hashTupel = GraphIsoPartitioner.HashTupel()
         hashBag = GraphIsoPartitioner.HashBag()
-        while(not self.checkEqualityRelation(colourPrevious, colour)):
+        equalityRelation = False
+        while(not equalityRelation):
             colourPrevious = colour
             colour = colourPrevious.copy()
-            for statement in graph.triples():
-                if(isinstance(statement[0], rdflib.BNode)):
-                    c = hashTupel.hash(colourPrevious[statement[1]],
-                                       colourPrevious[statement[2]])
-                    colour[statement[0]] = hashBag.hash(c,
-                                                        colour[statement[0]])
-                elif(isinstance(statement[2], rdflib.BNode)):
-                    c = hashTupel.hash(colourPrevious[statement[0]],
-                                       colourPrevious[statement[1]])
-                    colour[statement[2]] = hashBag.hash(c,
-                                                        colour[statement[2]])
+            for subj, pred, obje in graph:
+                if(isinstance(subj, rdflib.BNode)):
+                    c = hashTupel.hashT(colourPrevious[pred],
+                                        colourPrevious[obje])
+                    colour[subj] = hashBag.hashB(c, colour[subj])
+                elif(isinstance(obje, rdflib.BNode)):
+                    c = hashTupel.hashT(colourPrevious[subj],
+                                        colourPrevious[pred])
+                    colour[obje] = hashBag.hashB(c, colour[obje])
+            equalityRelation = self.checkEqualityRelation(colourPrevious,
+                                                          colour)
         return colour
 
     def checkEqualityRelation(self, colourPrevious, colourNext):
-        for key in colourPrevious:
-            if(colourPrevious[key] != colourNext[key]):
-                return False
+        for key1 in colourNext:
+            for key2 in colourNext:
+
+                # A<->B = A->B and B->A = -A or B and -B or A
+                if not ((colourPrevious[key1] != colourPrevious[key2]
+                         or colourNext[key1] == colourNext[key2])
+                        and (colourNext[key1] != colourNext[key2]
+                             or colourPrevious[key1] == colourPrevious[key2])
+                        ):
+                    return False
         return True
--- example.py
+++ example.py
@@ -11,9 +11,31 @@ slicer1 = atomicGraph.GraphSlicer(graph1)
 slicer1.run()
 slicer2 = atomicGraph.GraphSlicer(graph2)
 slicer2.run()
-
-partition = coloring.GraphIsoPartitioner()
 atomic = next(iter(slicer1.getAtomicGraphs()))
-partition.colour(atomic)
 
 print(slicer1 == slicer2)
+
+graphIso1 = rdflib.Graph()
+graphIso2 = rdflib.Graph()
+graphIso1 = graphIso1.parse("isoSimpleGraph1.ttl", format="n3")
+graphIso2 = graphIso2.parse("isoSimpleGraph2.ttl", format="n3")
+colouringAlgorithm = coloring.GraphIsoPartitioner()
+
+
+def compareColourMap(graph1, colourMap1, graph2, colourMap2):
+    print("started comparing")
+    for subj, pred, obje in graph1:
+        print("{} <> {} <> {}".format(subj, pred, obje))
+        print("{} ==> {} ==> {}\n<<<".format(colourMap1[subj],
+                                             colourMap1[pred],
+                                             colourMap1[obje]))
+    print("=============================================")
+    for subj, pred, obje in graph2:
+        print("{} <> {} <> {}".format(subj, pred, obje))
+        print("{} ==> {} ==> {}\n<<<".format(colourMap2[subj],
+                                             colourMap2[pred],
+                                             colourMap2[obje]))
+
+
+compareColourMap(graphIso1, colouringAlgorithm.colour(graphIso1),
+                 graphIso2, colouringAlgorithm.colour(graphIso2))
