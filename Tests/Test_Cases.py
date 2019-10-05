0d0b77a1cb052c158e4bada5399289b52d0959be
username <sebastianz541@googlemail.com>
2019-10-05 20:44:18 +0200

    added tests

---
+++ Tests/Test_Cases.py
@@ -0,0 +1,57 @@
+import rdflib
+import unittest
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import coloring
+import atomicGraph
+from Benchmark import graphconverter
+
+
+class TestIsomorphism(unittest.TestCase):
+    def setUp(self):
+        self.log = open("Log.txt", "w+")
+
+    def tearDown(self):
+        self.log.close()
+
+    def testSimpleIsomorphism(self):
+        g0 = rdflib.Graph()
+        g1 = rdflib.Graph()
+        graph0 = g0.parse("../Examples/example1.ttl", format="n3")
+        graph1 = g1.parse("../Examples/example2.ttl", format="n3")
+        isoAlgorithm = coloring.IsomorphicPartitioner()
+        isoPartition0 = isoAlgorithm.partitionIsomorphic(graph0)
+        isoPartition1 = isoAlgorithm.partitionIsomorphic(graph1)
+        self.assertTrue(isoPartition0 == isoPartition1)
+
+    def testIsomorphism(self):
+        successes = 0
+        fails = 0
+        for path, subdirs, files in os.walk("../../AtomicGraph Files/eval/"):
+            for file in files:
+                graph0 = graphconverter.convertGraphToRDF(os.path.join(path, file))
+                graph1 = graphconverter.convertGraphToRDF(os.path.join(path, file))
+                isoAlgorithm = coloring.IsomorphicPartitioner()
+                isoPartition0 = isoAlgorithm.partitionIsomorphic(graph0)
+                isoPartition1 = isoAlgorithm.partitionIsomorphic(graph1)
+                result = (isoPartition0 == isoPartition1)
+                if(not result):
+                    # show what went wrong
+                    msg = "{}\n==\n{}".format(isoPartition0, isoPartition1)
+                    self.log.write(msg)
+                    fails += 1
+                else:
+                    successes += 1
+                try:
+                    self.assertTrue(result)
+                except AssertionError:
+                    msg = "successes: {}  fails: {}".format(successes, fails)
+                    self.log.write(msg)
+                    raise
+        msg = "successes: {}  fails: {}".format(successes, fails)
+        self.log.write(msg)
+        self.log.close()
