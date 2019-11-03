699eb8366f5fecc15ef6ec1dddc12863b21c8ccf
username <sebastianz541@googlemail.com>
2019-11-03 15:16:22 +0100

    add set operators to comparableGraph

--- comp_graph.py
+++ comp_graph.py
@@ -4,14 +4,18 @@ import coloring
 
 
 class ComparableGraph(rdflib.Graph):
-    def __init__(self, graph):
-        super()
-        slicer = atomic_graph.GraphSlicer(graph)
-        slicer.run()
-        self.atomicGraphs = set()
-        partitioner = coloring.IsomorphicPartitioner()
-        for aGraph in slicer.getAtomicGraphs():
-            self.atomicGraphs.add(ComparableGraph.AtomicHashGraph(aGraph, partitioner))
+    def __init__(self, graph, atomicGraphs=None):
+        super(ComparableGraph, self).__init__(graph.store)
+        self.graph = graph
+        if atomicGraphs is None:
+            slicer = atomic_graph.GraphSlicer(graph)
+            slicer.run()
+            self.atomicGraphs = set()
+            partitioner = coloring.IsomorphicPartitioner()
+            for aGraph in slicer.getAtomicGraphs():
+                self.atomicGraphs.add(ComparableGraph.AtomicHashGraph(aGraph, partitioner))
+        else:
+            self.atomicGraphs = atomicGraphs
 
     def __eq__(self, value):
         if(issubclass(value.__class__, ComparableGraph)):
@@ -25,6 +29,32 @@ class ComparableGraph(rdflib.Graph):
         if(issubclass(value.__class__, rdflib.Graph)):
             return self == ComparableGraph(value)
         return False
+
+    def __add__(self, other):
+        result = ComparableGraph(self.graph.__or__(other.graph),
+                                 self.atomicGraphs.__or__(other.atomicGraphs))
+        return result
+
+    def __sub__(self, other):
+        result = ComparableGraph(self.graph.__sub__(other.graph),
+                                 self.atomicGraphs.__sub__(other.atomicGraphs))
+        return result
+
+    def __or__(self, other):
+        return self.__add__(other)
+
+    def __and__(self, other):
+        return self.__mul__(other)
+
+    def __mul__(self, other):
+        result = ComparableGraph(self.graph.__mul__(other.graph),
+                                 self.atomicGraphs.__and__(other.atomicGraphs))
+        return result
+
+    def __xor__(self, other):
+        result = ComparableGraph(self.graph.__xor__(other.graph),
+                                 self.atomicGraphs.__xor__(other.atomicGraphs))
+        return result
 
     class AtomicHashGraph:
         def __init__(self, atomicGraph, isomorphicPartitioner):
