21c9336e54e990825ce7e0441a7b94f666f91ca0
Natanael Arndt <arndtn@gmail.com>
2019-12-03 10:58:27 +0100

    Fix description in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+"""
+AtomicGraphs
+------------
+This is a python library to split an RDF Graph into atomic graphs.
+
+With this library using atomic graphs we want to make RDF Graphs containing
+blanknodes comparable.
+This package implements a colouring algorithm.
+"""
 from setuptools import setup
 
 setup(
-    name='atomicgraph',
-    version='1.0',
-    description=("This package provides a colouring algorithm,"
-                 " to make rdf-graphs containing blanknodes comparable."
-                 ),
+    name='atomicgraphs',
+    version='0.1.1',
+    description=("This is a library to split an RDF Graph into atomic graphs."),
+    long_description=__doc__,
     author='Simaris',
     # author_email='foomail@foo.com',
     packages=['atomicgraph'],
