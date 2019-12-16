24d92ff19ca0bc673eb131d5e4931cc2dd1c467c
Natanael Arndt <arndtn@gmail.com>
2019-12-16 16:39:26 +0100

    Some minor improvements to imports and an inline comment

--- atomicgraphs/atomic_store.py
+++ atomicgraphs/atomic_store.py
@@ -4,6 +4,8 @@ from rdflib.plugins import memory
 class AtomicStore(memory.IOMemory):
     def switchID(self, graph):
         id = self._IOMemory__obj2int[graph]
+        # The switchOnHash method changes the way __hash__() is implemented
+        # So we just rewrite the _IOMemory__obj2int and _IOMemory__int2obj structures
         graph.switchOnHash()
         self._IOMemory__obj2int[graph] = id
         self._IOMemory__int2obj[id] = graph
