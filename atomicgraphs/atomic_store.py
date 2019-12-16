d90303b8665c68d8b74522eccaf5f4957792ecf8
Natanael Arndt <arndtn@gmail.com>
2019-12-16 16:41:10 +0100

    Remive the graph obj to id entry during switch id operation

--- atomicgraphs/atomic_store.py
+++ atomicgraphs/atomic_store.py
@@ -4,6 +4,7 @@ from rdflib.plugins import memory
 class AtomicStore(memory.IOMemory):
     def switchID(self, graph):
         id = self._IOMemory__obj2int[graph]
+        self._IOMemory__obj2int.remove(graph)
         # The switchOnHash method changes the way __hash__() is implemented
         # So we just rewrite the _IOMemory__obj2int and _IOMemory__int2obj structures
         graph.switchOnHash()
